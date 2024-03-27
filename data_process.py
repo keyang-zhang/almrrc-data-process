1e9927b60b19b201e85a8c146badeb705471dd99
Keyang Zhang <keyangzhang@outlook.com>
2024-03-27 19:15:15 +0000

    collect all four depots data in Chicago; add stop info

--- data_process.py
+++ data_process.py
@@ -27,10 +27,11 @@ package_data_file = "./almrrc2021/almrrc2021-data-training/model_build_inputs/pa
 route_data_file = "./almrrc2021/almrrc2021-data-training/model_build_inputs/route_data.json"
 travel_time_data_file = "./almrrc2021/almrrc2021-data-training/model_build_inputs/travel_times.json"
 save_folder = "./instances/Chicago-all"
-selected_depot_ids = ['DCH1', 'DCH2', 'DCH3']
+selected_depot_ids = ['DCH1', 'DCH2', 'DCH3', 'DCH4']
 depot_coordinates = {'DCH1': (41.84032828797428, -87.68433006138673),  # (lat, long)
                      'DCH2': (42.031472230740874, -87.77709166137727),
-                     'DCH3': (41.803348036444014, -88.09727436138859)}
+                     'DCH3': (41.803348036444014, -88.09727436138859),
+                     'DCH4': (42.264654546897, -88.00377479386637)}
 
 with open(package_data_file, "r") as f:
     package_data_all = json.load(f)
@@ -55,7 +56,7 @@ for route_id, route in route_data_all.items():
 customer_info = {"customer_id": [], "dropoff_long": [], "dropoff_lat": [], "dropoff_node_id": [],
                  "volume": [], "dropoff_tw_start": [], "dropoff_tw_end": [], "service_duration": [],
                  "depth": [], "height": [], "width": [],
-                 "dropoff_zone_id": [], "delivered_date": [], "depot_id": [], "vehicle_id": []}
+                 "dropoff_zone_id": [], "delivered_date": [], "depot_id": [], "vehicle_id": [], "stop_id": []}
 customer_count = 0
 node_count = 0
 tt_matrix = {}
@@ -90,6 +91,7 @@ for route_id in package_data_all:
                 customer_info["volume"].append(d * h * w)
                 customer_info["depot_id"].append(depot_id)
                 customer_info["vehicle_id"].append(route_id_to_vehicle_id[route_id])
+                customer_info["stop_id"].append(stop_id)
                 customer_count += 1
         # update travel time matrix
         for orig_id in route_node_stop_map:
@@ -103,6 +105,13 @@ for route_id in package_data_all:
 # convert the data to tabular format
 pd.DataFrame(customer_info).to_csv(f"{save_folder}/customers.csv", index=False)
 pd.DataFrame(vehicle_info).to_csv(f"{save_folder}/vehicles.csv", index=False)
+
+# create depot file
+depot_info = {}
+depot_info["depot_id"] = list(depot_coordinates.keys())
+depot_info["long"] = [coord[1] for coord in depot_coordinates.values()]
+depot_info["lat"] = [coord[0] for coord in depot_coordinates.values()]
+pd.DataFrame(depot_info).to_csv(f"{save_folder}/depots.csv", index=False)
 
 # save distance matrix to json
 with open(f"{save_folder}/tt_matrix.json", "w") as f:
@@ -134,14 +143,25 @@ area_width = geo_distance(cust_lat_max, cust_long_max, cust_lat_max, cust_long_m
 area_depth = geo_distance(cust_lat_max, cust_long_max, cust_lat_min, cust_long_max)
 area_size = area_depth * area_depth
 
+# average capacity of vehicles
 vehicle_capacity_avg = sum(vehicle_info['capacity']) / vehicle_count
+
+# average number of stops vehicle routes made
+total_stop_count = sum(
+    [len(package_data_all[i]) for i in package_data_all if route_data_all[i]["station_code"] in selected_depot_ids])
+avg_stop_per_route = total_stop_count / vehicle_count
+
+# average number of parcels per stop
+avg_cust_per_stop = customer_count / total_stop_count
 
 with open(f"{save_folder}/statistics.txt", "w") as f:
     res_str = ""
+    res_str += f"Size of study area (km^3): {area_size} \n"
     for depot_id in depot_avg_dist_to_cust:
         avg_dist = depot_avg_dist_to_cust[depot_id]
         res_str += f"Average distance from depot {depot_id} to all customers (km): {avg_dist} \n"
     res_str += f"Average size of parcels (cm3): {cust_average_volume} \n"
     res_str += f"Average capacity of vehicles (cm3): {vehicle_capacity_avg} \n"
-    res_str += f"Size of study area (km^3): {area_size} \n"
+    res_str += f"Average number of stops per vehicle route: {avg_stop_per_route} \n"
+    res_str += f"Average number of parcels delivered per stop: {avg_cust_per_stop} \n"
     f.write(res_str)
